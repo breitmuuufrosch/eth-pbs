8575ea9e884c86e46e4f911b237937d4a1ac2607
alexus37 <alexlelidis@gmx.de>
2017-10-19 00:20:47 +0200

    Write report and create error graphs

--- exercises/ex2/report/make.sh
+++ exercises/ex2/report/make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # this will generate a document with question marks in place of unknown references
-pdflatex ex1.tex
+pdflatex ex.tex
 
 # this will parse all the .bib files that were included in the article and generate metainformation regarding references
-bibtex ex1
+bibtex ex
 
 # this will generate document with all the references in the correct places
-pdflatex ex1.tex
+pdflatex ex.tex
 
-echo "The pdf file ex1.pdf was created."
+echo "The pdf file ex.pdf was created."
