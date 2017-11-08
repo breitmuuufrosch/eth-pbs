3e222cec14e5f06f463ef873d4ab5316dcebc455
Andreas Emch <andreas.emch@bluewin.ch>
2017-11-08 09:08:46 +0100

    Final version: Corrected advection

--- exercises/ex3/ex3_code_framework/exercise.cpp
+++ exercises/ex3/ex3_code_framework/exercise.cpp
@@ -8,6 +8,11 @@
 #include "Utilities/Vector2T.h"
 #include "Utilities/Matrix2x2T.h"
 
+// We implemented two different versions of the bilinear-interpolation.
+// True:  1st method: Define matrices and use the matrix-multiplication (matrices derived from wikipedia-article)
+// False: 2nd method: First linearly interpolate the x-coordinates and use these values for the y-interpolation
+bool useMatrixForInterpolation = true;
+
 
 /**
  * \brief Clamping a value between an lower- and upper-bound, so that "lower <= value <= upper".
@@ -22,6 +27,7 @@ T Clamp(T _value, T _min, T _max) {
 	_value = (_max < _value) ? _max : _value;
 	return _value;
 }
+
 
 /**
  * \brief Safe access on array. Clamp the invalid index to a valid index "0 <= i < size".
@@ -38,6 +44,7 @@ double Safe(Array2d &_array, int x, int y) {
 
 	return _array(x, y);
 }
+
 
 /**
  * \brief Clamp a point within [0, 1]^2 to [dx, 1-dx]*[dy, 1-dy].
@@ -76,16 +83,18 @@ void ExSolvePoisson(int _xRes, int _yRes, int _iterations, double _accuracy, Arr
 	double dx = 1.0 / _xRes;
 	int gridCells = (_xRes - 2) * (_yRes - 2);
 
+	// Repeat for the number of iterations given. Loop is stopped as soon as solution converged.
 	for (int it = 0; it < _iterations; ++it) {
 		double residual = 0.0;
 
-		// Note that the boundaries are handles by the framework, so you iterations should be similar to:
+		// Update each grid, except the boundaries.
 		for (int y = 1; y < _yRes - 1; y++) {
 			for (int x = 1; x < _xRes - 1; x++) {
-				// Todo: dx??
 				_field(x, y) = (dx * dx * _b(x, y) + _field(x - 1, y) + _field(x, y - 1) + _field(x + 1, y) + _field(x, y + 1)) * 0.25;
 			}
 		}
+
+		// Calculate the residual to check if the solution converged.
 		for (int y = 1; y < _yRes - 1; y++) {
 			for (int x = 1; x < _xRes - 1; x++) {
 				double rij = (4.0 * _field(x, y) - _field(x - 1, y) - _field(x + 1, y) - _field(x, y - 1) - _field(x, y + 1)) / (dx * dx);
@@ -96,11 +105,12 @@ void ExSolvePoisson(int _xRes, int _yRes, int _iterations, double _accuracy, Arr
 		residual = std::sqrt(residual) / gridCells;
 
 
-		// For your debugging, and ours, please add these prints after every iteration
+		// Print the residual for the last iteration for the not converged solution.
 		if (it == _iterations - 1) {
 			printf("Pressure solver: it=%d, res=%f \n", it, residual);
 		}
 
+		// Print the residual and stop the calculation as soon as the solution converged.
 		if (residual < _accuracy) {
 			printf("Pressure solver: it=%d, res=%f, converged \n", it, residual);
 
@@ -108,6 +118,7 @@ void ExSolvePoisson(int _xRes, int _yRes, int _iterations, double _accuracy, Arr
 		}
 	}
 }
+
 
 /**
 * \brief Problem 2: Correct the velocities
@@ -131,16 +142,17 @@ void ExCorrectVelocities(int _xRes, int _yRes, double _dt, const Array2d &_press
 	double dy = 1.0 / _yRes;
 	double rho = 1.0;
 
-	// Note: velocity u_{i+1/2} is practically stored at i+1
+	// Update each edge/cell where the velocities are stored
 	for (int y = 0; y < _yRes - 1; ++y) {
 		for (int x = 0; x < _xRes - 1; ++x) {
+			// Formula is based on the exercise-slides, with i+1/2 => i+1
 			_xVelocity(x + 1, y) = _xVelocity(x + 1, y) - _dt / rho * (_pressure(x + 1, y) - _pressure(x, y)) / dx;
 			_yVelocity(x, y + 1) = _yVelocity(x, y + 1) - _dt / rho * (_pressure(x, y + 1) - _pressure(x, y)) / dy;
 		}
 	}
 }
 
-// Problem 3
+
 /**
  * \brief Bilinear interpolation for a given point with the 4 closest known values.
  * 
@@ -156,24 +168,38 @@ void ExCorrectVelocities(int _xRes, int _yRes, double _dt, const Array2d &_press
  *      Array with the needed units to advect.
  */
 double BilinearInterpolation(double dx, double dy, Vector2T<int> &q11_i, Vector2T<double> &p, Array2d &_array) {
+	// Calculate the position of the lower left and upper right grid-positions in the space [0, 1]^2.
 	Vector2T<double> q11(dx * q11_i.x(), dy * q11_i.y());
 	Vector2T<double> q22(q11.x() + dx, q11.y() + dy);
-	
-	// Todo: Personaly, I prefer the bilinear-interpolation as matrix-multiplication. If you agree we can keep it, otherwise the other code is still there below.
-	// The error between the two methods is around 1e-25, which leads visually to the same result.
-	double factor = 1.0 / ((q22.x() - q11.x()) * (q22.y() - q11.y()));
-	Vector2T<double> x_weights(q22.x() - p.x(), p.x() - q11.x());
-	Vector2T<double> y_weights(q22.y() - p.y(), p.y() - q11.y());
-	Matrix2x2T<double> f_values(Safe(_array, q11_i.x(), q11_i.y()), Safe(_array, q11_i.x(), q11_i.y() + 1), Safe(_array, q11_i.x() + 1, q11_i.y()), Safe(_array, q11_i.x() + 1, q11_i.y() + 1));
+	double value;
 
-	return factor * x_weights | (f_values * y_weights);
+	if (useMatrixForInterpolation) {
+		// Bilinear-interpolation with the matrix-formula which is derived on/from the wikipedia-article
+		// Source: (https://en.wikipedia.org/wiki/Bilinear_interpolation#Algorithm)
 
-	//double f_x_y1 = (q22.x() - p.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x(), q11_i.y())
-	//	+ (p.x() - q11.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x() + 1, q11_i.y());
-	//double f_x_y2 = (q22.x() - p.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x(), q11_i.y() + 1)
-	//	+ (p.x() - q11.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x() + 1, q11_i.y() + 1);
+		// Create vectors and matrices
+		double factor = 1.0 / ((q22.x() - q11.x()) * (q22.y() - q11.y()));
+		Vector2T<double> x_weights(q22.x() - p.x(), p.x() - q11.x());
+		Vector2T<double> y_weights(q22.y() - p.y(), p.y() - q11.y());
+		Matrix2x2T<double> f_values(Safe(_array, q11_i.x(), q11_i.y()), Safe(_array, q11_i.x(), q11_i.y() + 1), Safe(_array, q11_i.x() + 1, q11_i.y()), Safe(_array, q11_i.x() + 1, q11_i.y() + 1));
 
-	//return (q22.y() - p.y()) / (q22.y() - q11.y()) * f_x_y1 + (p.y() - q11.y()) / (q22.y() - q11.y()) * f_x_y2;
+		// Calculate the interpolation directly
+		value = factor * x_weights | (f_values * y_weights);
+	} else {
+		// Bilinear-interpolation with the first three steps on the wikipedia-article
+		// Source: (https://en.wikipedia.org/wiki/Bilinear_interpolation#Algorithm)
+
+		// Linear interpolation along x-axis:
+		double f_x_y1 = (q22.x() - p.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x(), q11_i.y())
+			+ (p.x() - q11.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x() + 1, q11_i.y());
+		double f_x_y2 = (q22.x() - p.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x(), q11_i.y() + 1)
+			+ (p.x() - q11.x()) / (q22.x() - q11.x()) * Safe(_array, q11_i.x() + 1, q11_i.y() + 1);
+
+		// Linear interpolation along y-axis:
+		value = (q22.y() - p.y()) / (q22.y() - q11.y()) * f_x_y1 + (p.y() - q11.y()) / (q22.y() - q11.y()) * f_x_y2;
+	}
+
+	return value;
 }
 
 
@@ -204,38 +230,61 @@ void ExAdvectWithSemiLagrange(int _xRes, int _yRes, double _dt, Array2d &_xVeloc
 	double dx = 1.0 / _xRes;
 	double dy = 1.0 / _yRes;
 
-	// Note: velocity u_{i+1/2} is practically stored at i+1
-	for (int y = 1; y < _yRes - 1; y++) {
-		for (int x = 1; x < _xRes - 1; x++) {
-			Vector2T<double> u_x((_xVelocity(x, y) + _xVelocity(x+1, y)) / 2.0, (_yVelocity(x, y) + _yVelocity(x, y + 1)) / 2.0);
+	// Advect x-velocity
+	for (int y = 0; y < _xVelocity.size(1)-1; y++) {
+		for (int x = 0; x < _xVelocity.size(0)-1; x++) {
+			Vector2T<double> u_x(_xVelocity(x, y), (_yVelocity(x, y) + _yVelocity(x, y + 1)) / 2.0);
 			Vector2T<double> p(x * dx, y * dy);
 			Vector2T<double> Xp_velocity = p - _dt * u_x;
-			Vector2T<double> Xp_density = Xp_velocity;
-			
-			//Todo: Please re-check.. when clamping is active, the pillar thends to the left side after some time..
-			Clamp(Xp_velocity, 2.5*dx, 2.5*dy);
-			Clamp(Xp_density, 2.0*dx, 2.0*dy);
+
+			//Todo: If clamping for velocity is done, the advection tends to go right, therefore we do not clamp those, only the density.
+			//Clamp(Xp_velocity, 2.5*dx, 2.5*dy);
 
 			int indexV_x0 = std::floor(Xp_velocity.x() * _xRes);
 			int indexV_y0 = std::floor(Xp_velocity.y() * _yRes);
 			Vector2T<int> q11_velocity(indexV_x0, indexV_y0);
 
+			_xVelocityTemp(x, y) = BilinearInterpolation(dx, dy, q11_velocity, Xp_velocity, _xVelocity);
+		}
+	}
+
+	// Advect y-velocity
+	for (int y = 0; y < _yVelocity.size(1)-1; y++) {
+		for (int x = 0; x < _yVelocity.size(0)-1; x++) {
+			Vector2T<double> u_x((_xVelocity(x, y) + _xVelocity(x + 1, y)) / 2.0, _yVelocity(x, y));
+			Vector2T<double> p(x * dx, y * dy);
+			Vector2T<double> Xp_velocity = p - _dt * u_x;
+
+			//Todo: If clamping for velocity is done, the advection tends to go right, therefore we do not clamp those, only the density.
+			//Clamp(Xp_velocity, 2.5*dx, 2.5*dy);
+
+			int indexV_x0 = std::floor(Xp_velocity.x() * _xRes);
+			int indexV_y0 = std::floor(Xp_velocity.y() * _yRes);
+			Vector2T<int> q11_velocity(indexV_x0, indexV_y0);
+
+			_yVelocityTemp(x, y) = BilinearInterpolation(dx, dy, q11_velocity, Xp_velocity, _yVelocity);
+		}
+	}
+
+	// Advect density
+	for (int y = 1; y < _yRes - 1; y++) {
+		for (int x = 1; x < _xRes - 1; x++) {
+			Vector2T<double> u_x((_xVelocity(x, y) + _xVelocity(x + 1, y)) / 2.0, (_yVelocity(x, y) + _yVelocity(x, y + 1)) / 2.0);
+			Vector2T<double> p(x * dx, y * dy);
+			Vector2T<double> Xp_density = p - _dt * u_x;
+
+			Clamp(Xp_density, 2.0*dx, 2.0*dy);
+
 			int indexD_x0 = std::floor(Xp_density.x() * _xRes);
 			int indexD_y0 = std::floor(Xp_density.y() * _yRes);
 			Vector2T<int> q11_density(indexD_x0, indexD_y0);
 
-			_xVelocityTemp(x, y) = BilinearInterpolation(dx, dy, q11_velocity, Xp_velocity, _xVelocity);
-			_yVelocityTemp(x, y) = BilinearInterpolation(dx, dy, q11_velocity, Xp_velocity, _yVelocity);
-			_densityTemp(x, y)   = BilinearInterpolation(dx, dy, q11_density, Xp_density, _density);
+			_densityTemp(x, y) = BilinearInterpolation(dx, dy, q11_density, Xp_density, _density);
 		}
 	}
 
-	// Copy results from tmp buffers. (do not copy the boarders since we did not update them)
-	for (int y = 1; y < _yRes - 1; y++) {
-		for (int x = 1; x < _xRes - 1; x++) {
-			_density(x, y)   = _densityTemp(x, y);
-			_xVelocity(x, y) = _xVelocityTemp(x, y);
-			_yVelocity(x, y) = _yVelocityTemp(x, y);
-		}
-	}
+	// Copy results from tmp buffers
+	_density = _densityTemp;
+	_xVelocity = _xVelocityTemp;
+	_yVelocity = _yVelocityTemp;
 }
