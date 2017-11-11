e6b88e440b31d8684d857d6bd61e13a7fc18f923
Andreas Emch <andreas.emch@bluewin.ch>
2017-11-11 13:06:42 +0100

    Corrected path for linux and osg-lib

--- project/code/config.h
+++ project/code/config.h
@@ -11,6 +11,6 @@
 #define VERSION ""
 #define NUMBER  
 
-const std::string DATA_PATH = "D:/Clouds/OneDrive/Education/ETH/5. semester/Physically-Based Simulation in Computer Graphics/project/code/data";
+const std::string DATA_PATH = "D:/Clouds/OneDrive/Education/ETH/5. semester/Physically-Based Simulation in Computer Graphics/physSim/project/code/data";
 
 #endif /* EXAMPLE_H */
